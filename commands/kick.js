dd3a9a47288bc5e12d89b3b507097b6d561b3599
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-08-17 23:10:33 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
     message.reply("Weet je zeker dat je deze gebruiker wilt kicken?");
 
-    message.react('👍').then(() => message.react('👎'));
+    return message.react('👍').then(() => message.react('👎'));
 
     const filter = (reaction, user) => {
         return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
@@ -15,9 +15,9 @@ module.exports.run = async (bot, message, args) => {
             const reaction = collected.first();
     
             if (reaction.emoji.name === '👍') {
-                message.reply('Grapjee, xxx we love you');
+                message.channel.send('Grapjee, xxx we love you');
             } else {
-                message.reply('Ik stop zo die kanker duim in je moeders kut !');
+                message.channel.send('Ik stop zo die kanker duim in je moeders kut !');
             }
         })
         .catch(collected => {
