2a642aec3407d846b712c0ce50ac2b46095ad5f1
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-08-17 22:49:09 +0200

    Update online2.js

---
+++ commands/online2.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.guild.members.fetch().then(fetchedMembers => {
+        const totalOnline = fetchedMembers.filter(member => member.presence.status === 'online');
+        // We now have a collection with all online member objects in the totalOnline variable
+        message.channel.send({embed: {
+            color: 15158332,
+            description: `Er zijn momenteel **^2${totalOnline.size}** members online in Chappie's Clubhuis!`
+          }});
+    });
+
+
+}
+
+module.exports.help = {
+    name: "Online"
+}
