03b21d1b52293f44df592266cc8061a439b1da46
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-08-20 17:17:43 +0200

    Update hallo.js

--- commands/hallo.js
+++ commands/hallo.js
@@ -2,9 +2,12 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Hallo dikzak");
+    return message.channel.send({embed: { 
+        color: 10181046,
+        discription: "Hallo dikzak"}});
+        
 
-}
+    }
 
 module.exports.help = {
     name: "hallo"
