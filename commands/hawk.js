979f43f550c3de0cbcf53413e2c49f5b76347dad
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-08-23 20:12:03 +0200

    nn

---
+++ commands/hawk.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    return message.channel.send("Hawk is een pro, een legend, een toffe gast & natuurlijk vind ik het leuk om je helemaal de KANKER IN TE SCHELDEN !");
+
+}
+
+module.exports.help = {
+    name: "hawk"
+}
